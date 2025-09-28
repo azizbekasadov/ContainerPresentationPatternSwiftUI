5be7688282838ac8d37447bc96925d32f3d89608
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-09-28 23:39:53 +0200

    Initial Commit

---
+++ ContainerPresentationPatternSwiftUI/ContainerPresentationPatternSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerPresentationPatternSwiftUIApp.swift
+//  ContainerPresentationPatternSwiftUI
+//
+//  Created by Azizbek Asadov on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContainerPresentationPatternSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
